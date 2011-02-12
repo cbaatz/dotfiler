7a7714a222be1f70aded6d5557563e53d2f3e3a0
Carl Baatz <carl.baatz@gmail.com>
2011-02-12 11:04:22 +0000

    Add and restore functions for new conventions.

---
+++ dots.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+"""
+Dotfiler makes it easier to manage dot-files using symlinks to a
+separate directory. The dot-files you want to manage are symlinks to a
+file with the same name but without the leading dot in a separate
+dot-files directory. See README.org for usage.
+
+"""
+from __future__ import print_function
+import os
+import shutil
+import sys
+
+# Flush stdoutput immediately
+class flushfile(object):
+    def __init__(self, f):
+        self.f = f
+    def write(self, x):
+        self.f.write(x)
+        self.f.flush()
+sys.stdout = flushfile(sys.stdout)
+
+HOME_DIR = os.environ['HOME']
+
+def dot_dir():
+   return os.environ['PWD']
+
+class DirExistsException(Exception):
+    pass
+class NotManageableException(Exception):
+    pass
+class AlreadyManagedException(Exception):
+    pass
+class PartiallyManagedException(Exception):
+    pass
+class NotManagedException(Exception):
+    pass
+
+def undot(string):
+    return string.lstrip(".")
+
+def dot_to_target(path):
+    # todo exception if dir is not home
+    filename = os.path.basename(path)
+    if filename.startswith("."):
+        nakedname = filename[1:]
+#    else:
+#        raise NotManageableException("%s does not start with a dot.", filename)
+    return os.path.join(dot_dir(), nakedname)
+
+def target_to_dot(name):
+    pass
+
+def add(dot_path):
+    """
+    Add dot_path to managed dot-files by moving it to dot_dir without
+    leading dot, and creating a symlink from the home directory to
+    that.
+
+    """
+    if os.path.islink(dot_path):
+        if os.readlink(dot_path) == dot_to_target(dot_path):
+            raise AlreadyManagedException("%s is already a managed link", dot_path)
+        else:
+            raise NotManageableException("%s is a link.", dot_path)
+
+    target_path = dot_to_target(dot_path)
+    shutil.move(dot_path, target_path)
+    os.symlink(target_path, dot_path)
+    return target_path
+
+def restore(dot_path):
+    """
+    Restore dot_path to the home directory by moving it from the
+    dot-files directory.
+
+    """
+    if not os.path.exists(dot_path):
+        raise NotManagedException("%s does not exist.", dot_path)
+    elif not os.path.islink(dot_path):
+        raise NotManagedException("%s is not a link.", dot_path)
+    elif os.readlink(dot_path) != dot_to_target(dot_path):
+        raise NotManagedException(
+            "%s is not a link to the dot-files directory.", dot_path)
+    else:
+        os.remove(dot_path)
+        shutil.move(dot_to_target(dot_path), dot_path)
+
+def update(clean=True, overwrite=False):
+    """
+    Update home-dir symlinks for managed dot-files.
+
+    """
+    pass
+
+def do_status():
+    print("Status command not yet implemented.")
+
+def do_add(*files):
+    if len(files) < 1:
+        print("Usage: dots add DOTFILES")
+        print("Example: dots add ~/.emacs.d ~/.bashrc")
+    else:
+        for f in files:
+            print("Adding %s to managed files..." % f, end="")
+        try:
+            add(f)
+            print(" DONE.")
+        except NotManageableException as e:
+            print(" ERROR. %s" % e.message)
+        except AlreadyManagedException as e:
+            print(" ERROR. %s" % e.message)
+
+def do_restore(*files):
+    if len(files) < 1:
+        print("Usage: dots restore DOTFILES")
+        print("Example: dots restore ~/.emacs.d ~/.bashrc")
+    else:
+        for f in files:
+            print("Removing %s from management..." % f, end="")
+        try:
+            restore(f)
+            print(" DONE.")
+        except NotManagedException as e:
+            print(" ERROR. %s" % e.message)
+
+def main():
+    args = list(sys.argv)
+    program = args.pop(0)
+    command = None
+    if args:
+        command = args.pop(0)
+    files = []
+    for f in args:
+        files.append(os.path.abspath(f))
+
+    if command == None or command == "status":
+        do_status()
+    elif command == "add":
+        do_add(*files)
+    elif command == "restore":
+        do_restore(*files)
+
+    if command == None:
+        print("""\nAvailable commands:
+
+add     DOTFILES      Put DOTFILES under management
+restore DOTFILES      Remove DOTFILES from management
+update                Update symlinks in home dir
+""")
+
+if __name__ == "__main__":
+    main()
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@
 
 import unittest
 import random
-import dotfiler
+import dots
 
 import os
 import shutil
@@ -11,18 +11,20 @@ import tempfile
 class TempDirTestCase(unittest.TestCase):
     """
     Create and set temporary home and dot dirs.
-    
+
     """
     def setUp(self):
         super(TempDirTestCase, self).setUp()
-        self.orig_home_dir = dotfiler.HOME_DIR
+        self.orig_home_dir = dots.HOME_DIR
         self.home_dir = tempfile.mkdtemp()
-        self.dot_dir = os.path.join(self.home_dir, "thedotfiles")
-        dotfiler.HOME_DIR = self.home_dir
+        self.dot_dir = os.path.join(self.home_dir, "dotfiles")
+        os.mkdir(self.dot_dir)
+        os.environ['PWD'] = self.dot_dir
+        dots.HOME_DIR = self.home_dir
 
     def tearDown(self):
         super(TempDirTestCase, self).tearDown()
-        dotfiler.HOME_DIR = self.orig_home_dir
+        dots.HOME_DIR = self.orig_home_dir
 
 class GlobalTests(unittest.TestCase):
 
@@ -31,386 +33,88 @@ class GlobalTests(unittest.TestCase):
         HOME_DIR is set to the environment variable HOME.
 
         """
-        self.assertEqual(dotfiler.HOME_DIR, os.environ["HOME"])
+        self.assertEqual(dots.HOME_DIR, os.environ["HOME"])
 
-    def test_config_name(self):
-        """
-        CONFIG_NAME is ".dotfiles"
+    def test_dot_dir_current(self):
+        self.dot_dir = os.path.join(tempfile.mkdtemp(), "dotfiles")
+        os.environ['PWD'] = self.dot_dir
+        self.assertEqual(self.dot_dir, dots.dot_dir())
 
-        """
-        self.assertEqual(dotfiler.CONFIG_NAME, ".dotfiles")
+    def test_dot_to_target(self):
+        pass
 
-    def test_symlink_filename(self):
-        """
-        symlink_filename is HOME_DIR/CONFIG_NAME.
-
-        """
-        self.assertEqual(dotfiler.symlink_filename(),
-                         os.path.join(dotfiler.HOME_DIR, dotfiler.CONFIG_NAME))
-
-class IsManagedTests(TempDirTestCase):
-    def setUp(self):
-        """
-        Set up names.
-
-        """
-        super(IsManagedTests, self).setUp()
-        self.dot_dir = os.path.join(dotfiler.HOME_DIR, "dotdir")
-        dotfiler.init(self.dot_dir)
-
-    def test_missing(self):
-        self.setUp()
-        filename = os.path.join(self.home_dir, ".testfile1")
-        self.assertFalse(dotfiler.is_managed(filename))
-
+class AddTests(TempDirTestCase):
     def test_normal_file(self):
-        self.setUp()
-        filename = os.path.join(self.home_dir, ".testfile1")
-        open(filename, "w").close()
-        self.assertFalse(dotfiler.is_managed(filename))
+        dotname = os.path.join(self.home_dir, ".testfile")
+        open(dotname, "w").close()
+        targetname = os.path.join(dots.dot_dir(), "testfile")
 
-    def test_other_link(self):
-        self.setUp()
-        target = os.path.join(self.home_dir, "targetfile")
-        open(target, "w").close()
-        filename = os.path.join(self.home_dir, ".testfile1")
-        os.symlink(target, filename)
-        self.assertFalse(dotfiler.is_managed(filename))
+        self.assertFalse(os.path.islink(dotname))
+        self.assertFalse(os.path.exists(targetname))
+        result = dots.add(dotname)
+        self.assertTrue(os.path.islink(dotname))
+        self.assertTrue(os.path.isfile(targetname))
+        self.assertEqual(result, os.path.abspath(targetname))
+        self.assertEqual(os.path.abspath(os.readlink(dotname)),
+                         os.path.abspath(targetname))
 
-    def test_broken_link(self):
-        self.setUp()
-        filename = os.path.join(self.home_dir, ".testfile1")
-        open(filename, "w").close()
-        dotfiler.manage(filename)
-        os.remove(os.path.join(self.dot_dir, "testfile1"))
-        self.assertFalse(dotfiler.is_managed(filename))
+    def test_link_correct(self):
+        dotpath = os.path.join(self.home_dir, ".testfile")
+        targetpath = dots.dot_to_target(dotpath)
+        os.symlink(targetpath, dotpath)
 
-    def test_ok(self):
-        self.setUp()
-        filename = os.path.join(self.home_dir, ".testfile1")
-        open(filename, "w").close()
-        dotfiler.manage(filename)
-        self.assertTrue(dotfiler.is_managed(filename))
+        self.assertTrue(os.path.islink(dotpath))
+        self.assertEqual(os.path.abspath(os.readlink(dotpath)),
+                         os.path.abspath(targetpath))
 
-class UnmanageTests(TempDirTestCase):
-    def setUp(self):
-        """
-        Set up names.
+        self.assertRaises(dots.AlreadyManagedException, dots.add, dotpath)
 
-        """
-        super(UnmanageTests, self).setUp()
-        self.dot_dir = os.path.join(dotfiler.HOME_DIR, "dotdir")
-        dotfiler.init(self.dot_dir)
+        # Nothing should have changed
+        self.assertTrue(os.path.islink(dotpath))
+        self.assertEqual(os.path.abspath(os.readlink(dotpath)),
+                         os.path.abspath(targetpath))
 
+    def test_link_other(self):
+        dotpath = os.path.join(self.home_dir, ".testfile")
+        os.symlink("something", dotpath)
+
+        self.assertTrue(os.path.islink(dotpath))
+        self.assertEqual(os.path.abspath(os.readlink(dotpath)),
+                         os.path.abspath("something"))
+        self.assertRaises(dots.NotManageableException, dots.add, dotpath)
+        # Nothing should have changed
+        self.assertTrue(os.path.islink(dotpath))
+        self.assertEqual(os.path.abspath(os.readlink(dotpath)),
+                         os.path.abspath("something"))
+
+class RestoreTests(TempDirTestCase):
     def test_managed_file(self):
-        self.setUp()
-        # Create managed file
-        filename = os.path.join(self.home_dir, ".testfile1")
-        open(filename, "w").close()
-        # Manage file and check results
-        dotfiler.manage(filename)
-        self.assertTrue(dotfiler.is_managed(filename))
-        self.assertTrue(os.path.exists(os.path.join(self.dot_dir, "testfile1")))
-        # Unmanage file and check results
-        dotfiler.unmanage(filename)
-        self.assertFalse(dotfiler.is_managed(filename))
-        self.assertFalse(os.path.islink(filename))
-        self.assertFalse(os.path.islink(filename))
-        self.assertFalse(os.path.exists(os.path.join(self.dot_dir, "testfile1")))
+        dotpath = os.path.join(self.home_dir, ".testfile")
+        open(dotpath, "w").close()
+        targetpath = dots.add(dotpath)
+        self.assertTrue(os.path.exists(targetpath))
+        self.assertFalse(os.path.islink(targetpath))
+        self.assertTrue(os.path.islink(dotpath))
+        dots.restore(dotpath)
+        self.assertFalse(os.path.exists(targetpath))
+        self.assertFalse(os.path.islink(dotpath))
+        self.assertTrue(os.path.isfile(dotpath))
 
-    def test_managed_dir(self):
-        self.setUp()
-        # Create managed file
-        dirname = os.path.join(self.home_dir, ".testdir")
-        os.mkdir(dirname)
-        open(os.path.join(dirname, "testfile"), "w").close()
-        # Manage file and check results
-        dotfiler.manage(dirname)
-        self.assertTrue(dotfiler.is_managed(dirname))
-        self.assertTrue(os.path.exists(os.path.join(self.dot_dir, "testdir")))
-        # Unmanage file and check results
-        dotfiler.unmanage(dirname)
-        self.assertFalse(dotfiler.is_managed(dirname))
-        self.assertFalse(os.path.islink(dirname))
-        self.assertFalse(os.path.exists(os.path.join(self.dot_dir, "testdir")))
+    def test_unmanaged_file(self):
+        dotpath = os.path.join(self.home_dir, ".testfile")
+        open(dotpath, "w").close()
+        self.assertTrue(os.path.exists(dotpath))
+        self.assertFalse(os.path.islink(dotpath))
+        self.assertRaises(dots.NotManagedException, dots.restore, dotpath)
 
-    def test_managed_nested(self):
-        self.setUp()
-        # Create unmanaged dot directory and a managed file inside it.
-        dirname = os.path.join(self.home_dir, ".dir")
-        os.mkdir(dirname)
-        filename = os.path.join(dirname, "subdir/testfile1")
-        os.makedirs(os.path.dirname(filename))
-        open(filename, "w").close()
-        # Manage file and check results
-        dotfiler.manage(filename)
-        self.assertTrue(dotfiler.is_managed(filename))
-        self.assertTrue(dotfiler.is_partially_managed(dirname))
-        self.assertTrue(os.path.exists(
-                os.path.join(self.dot_dir, "dir/subdir/testfile1")))
-        dotfiler.unmanage(filename)
-        self.assertFalse(dotfiler.is_managed(filename))
-        self.assertFalse(dotfiler.is_partially_managed(dirname))
-        self.assertFalse(os.path.islink(filename))
-        self.assertFalse(os.path.exists(
-                os.path.join(self.dot_dir, "dir/subdir/testfile1")))
+    def test_nonexistent_file(self):
+        dotpath = os.path.join(self.home_dir, ".testfile")
+        self.assertFalse(os.path.exists(dotpath))
+        self.assertFalse(os.path.islink(dotpath))
+        self.assertRaises(dots.NotManagedException, dots.restore, dotpath)
 
-    def test_partially_managed(self):
-        self.setUp()
-        # Create partially managed dir
-        dirname = os.path.join(self.home_dir, ".dir")
-        os.mkdir(dirname)
-        filename = os.path.join(dirname, "subdir/subdir/testfile1")
-        os.makedirs(os.path.dirname(filename))
-        open(filename, "w").close()
-        dotfiler.manage(filename)
-        self.assertTrue(dotfiler.is_partially_managed(dirname))
-        dotfiler.unmanage(dirname)
-        self.assertFalse(dotfiler.is_partially_managed(dirname))
-        self.assertFalse(dotfiler.is_managed(dirname))
-
-    def test_unmanaged(self):
-        # Raise NotManagedException
-        filename1 = os.path.join(self.home_dir, ".testfile1")
-        open(filename1, "w").close()
-        filename2 = os.path.join(self.home_dir, ".testfile2")
-        self.assertRaises(dotfiler.NotManagedException, dotfiler.unmanage, filename1)
-        self.assertRaises(dotfiler.NotManagedException, dotfiler.unmanage, filename2)
-
-class ManageTests(TempDirTestCase):
-    def setUp(self):
-        """
-        Set up names.
-
-        """
-        super(ManageTests, self).setUp()
-        self.dot_dir = os.path.join(dotfiler.HOME_DIR, "dotdir")
-        dotfiler.init(self.dot_dir)
-
-    def test_manage(self):
-        self.setUp()
-        filename = os.path.join(self.home_dir, ".test1")
-        open(filename, "w").close()
-        target = dotfiler.manage(filename)
-        dot_dir = os.path.dirname(dotfiler.get_config().name)
-        self.assertEqual(os.path.dirname(target), dot_dir)
-        self.assertTrue(os.path.islink(filename))
-        self.assertEqual(os.readlink(filename), target)
-        self.assertTrue(os.path.exists(target))
-
-    def test_manage_nested_in_managed(self):
-        self.setUp()
-        dirname = os.path.join(self.home_dir, ".dir")
-        os.mkdir(dirname)
-        filename = os.path.join(dirname, "testfile")
-        open(filename, "w").close()
-        dotfiler.manage(dirname)
-        self.assertRaises(dotfiler.AlreadyManagedException, dotfiler.manage, filename)
-
-    def test_manage_nested(self):
-        self.setUp()
-        # Create unmanaged dot directory and a managed file inside it.
-        dirname = os.path.join(self.home_dir, ".dir")
-        managed_path = os.path.join(self.dot_dir, "dir", "testfile1")
-        os.mkdir(dirname)
-        filename1 = os.path.join(dirname, "testfile1")
-        filename2 = os.path.join(dirname, "testfile2")
-        open(filename1, "w").close()
-        open(filename2, "w").close()
-        dotfiler.manage(filename1)
-        self.assertTrue(dotfiler.is_managed(filename1))
-        self.assertTrue(os.path.exists(managed_path))
-        self.assertTrue(os.path.isfile(managed_path))
-        self.assertFalse(dotfiler.is_managed(dirname))
-        self.assertFalse(dotfiler.is_managed(filename2))
-        
-    def test_manage_dir_partial_simple(self):
-        self.setUp()
-        # Create unmanaged dot directory and a managed file inside it.
-        dirname = os.path.join(self.home_dir, ".dir")
-        os.mkdir(dirname)
-        filename = os.path.join(dirname, "testfile1")
-        open(filename, "w").close()
-        dotfiler.manage(filename)
-        # Managing .dir should now give a PartiallyManagedException
-        self.assertRaises(dotfiler.PartiallyManagedException,
-                          dotfiler.manage, dirname)
-        self.assertRaises(dotfiler.AlreadyManagedException,
-                          dotfiler.manage, filename)
-
-    def test_manage_dir_partial_subdir(self):
-        self.setUp()
-        # Create unmanaged dot directory and a managed file inside it.
-        dirname = os.path.join(self.home_dir, ".dir")
-        os.mkdir(dirname)
-        filename = os.path.join(dirname, "subdir/subdir/testfile1")
-        os.makedirs(os.path.dirname(filename))
-        open(filename, "w").close()
-        dotfiler.manage(filename)
-        # Managing .dir should now give a PartiallyManagedException
-        self.assertRaises(dotfiler.PartiallyManagedException,
-                          dotfiler.manage, dirname)
-        self.assertRaises(dotfiler.PartiallyManagedException,
-                          dotfiler.manage, os.path.join(dirname, "subdir"))
-        self.assertRaises(dotfiler.PartiallyManagedException,
-                          dotfiler.manage, os.path.join(dirname, "subdir/subdir"))
-        self.assertRaises(dotfiler.AlreadyManagedException,
-                          dotfiler.manage, filename)
-
-    def test_manage_not_home(self):
-        self.setUp()
-        self.other_dir = tempfile.mkdtemp()
-        filename = os.path.join(self.other_dir, ".testlink")
-        open(filename, "w").close()
-        self.assertRaises(dotfiler.NotManageableException, dotfiler.manage, filename)
-
-    def test_manage_managed(self):
-        self.setUp()
-        dirname = os.path.join(self.home_dir, ".testdir")
-        filename = os.path.join(self.home_dir, ".testfile")
-        os.mkdir(dirname)
-        open(filename, "w").close()
-        dotfiler.manage(dirname)
-        dotfiler.manage(filename)
-        self.assertRaises(dotfiler.AlreadyManagedException, dotfiler.manage, filename)
-        self.assertRaises(dotfiler.AlreadyManagedException, dotfiler.manage, dirname)
-
-    def test_manage_dotless(self):
-        self.setUp()
-        dirname = os.path.join(self.home_dir, "testdir")
-        filename = os.path.join(self.home_dir, "testfile")
-        os.mkdir(dirname)
-        open(filename, "w").close()
-        self.assertRaises(dotfiler.NotManageableException, dotfiler.manage, filename)
-        self.assertRaises(dotfiler.NotManageableException, dotfiler.manage, dirname)
-    
-    def test_manage_other_link(self):
-        self.setUp()
-        filename = os.path.join(self.home_dir, ".testlink")
-        os.symlink("/", filename)
-        self.assertRaises(dotfiler.NotManageableException, dotfiler.manage, filename)
-
-    def test_manage_missing(self):
-        self.setUp()
-        filename = os.path.join(self.home_dir, ".missing")
-        self.assertRaises(dotfiler.NotManageableException, dotfiler.manage, filename)
-
-class InitTests(TempDirTestCase):
-
-    def setUp(self):
-        """
-        Set up names.
-
-        """
-        super(InitTests, self).setUp()
-        self.symlink = dotfiler.symlink_filename()
-        self.dot_dir = os.path.join(dotfiler.HOME_DIR, "dotdir")
-        self.target = os.path.join(self.dot_dir, dotfiler.nodot(dotfiler.CONFIG_NAME))
-
-    def init_files(self):
-        """
-        Remove symlink and dot_dir in (temporary) home dir.
-
-        """
-        try:
-            os.remove(self.symlink)
-        except OSError:
-            pass
-        try:
-            shutil.rmtree(self.dot_dir)
-        except OSError:
-            pass
-
-    def test_existing_file(self):
-        self.init_files()
-        f = open(self.symlink, "w")
-        f.close()
-        self.assertRaises(dotfiler.ConfigExistsException, dotfiler.init, self.dot_dir)
-        self.assertFalse(os.path.exists(self.dot_dir))
-        
-    def test_existing_dir(self):
-        self.init_files()
-        os.makedirs(self.dot_dir)
-        self.assertRaises(dotfiler.DirExistsException, dotfiler.init, self.dot_dir)
-        self.assertFalse(os.path.exists(self.symlink))
-
-    def test_ok(self):
-        self.init_files()
-        dotfiler.init(self.dot_dir)
-        f = dotfiler.get_config()
-        self.assertIsInstance(f, file)
-        self.assertEquals(f.name, self.target)
-
-class GetConfigTests(TempDirTestCase):
-
-    def setUp(self):
-        """
-        Set up names.
-
-        """
-        super(GetConfigTests, self).setUp()
-        self.symlink = dotfiler.symlink_filename()
-        self.dot_dir = os.path.join(dotfiler.HOME_DIR, "dotdir")
-        self.target = os.path.join(self.dot_dir, "dotfiles")
-
-    def init_files(self):
-        """
-        Remove symlink and dot_dir in (temporary) home dir and create
-        empty dot_dir.
-
-        """
-        try:
-            os.remove(self.symlink)
-        except OSError:
-            pass
-        try:
-            shutil.rmtree(self.dot_dir)
-        except OSError:
-            pass
-
-        os.makedirs(self.dot_dir)
-
-    def create_normal_file(self):
-        f = open(self.symlink, "w")
-        f.close()
-        
-    def create_symlink(self):
-        os.symlink(self.target, self.symlink)
-    
-    def create_dir_target(self):
-        os.makedirs(self.target)
-
-    def create_target(self):
-        f = open(self.target, "w")
-        f.close()
-
-    def test_nothing(self):
-        self.init_files()
-        self.assertRaises(dotfiler.ConfigMissingException, dotfiler.get_config)
-
-    def test_no_target(self):
-        self.init_files()
-        self.create_symlink()
-        self.assertRaises(dotfiler.ConfigMissingException, dotfiler.get_config)
-
-    def test_normal(self):
-        self.init_files()
-        self.create_normal_file()
-        self.assertRaises(dotfiler.ConfigInvalidException, dotfiler.get_config)
-
-    def test_dir_target(self):
-        self.init_files()
-        self.create_symlink()
-        self.create_dir_target()
-        self.assertRaises(dotfiler.ConfigInvalidException, dotfiler.get_config)
-
-    def test_ok(self):
-        self.init_files()
-        self.create_symlink()
-        self.create_target()
-        f = dotfiler.get_config()
-        self.assertIsInstance(f, file)
-        self.assertEquals(f.name, self.target)
+class UpdateTests(TempDirTestCase):
+    pass
 
 if __name__ == "__main__":
     unittest.main()
